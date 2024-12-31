332fb312db16858aa241a01ce34dd7597dd4f9d3
wasimGhoniem <wasimghoniem@gmail.com>
2024-12-31 20:12:21 +0200

    Changed text in  appBar in scaffold.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('git'),
+        title: Text('git test'),
       ),
     );
   }
