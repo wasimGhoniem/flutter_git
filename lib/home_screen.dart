9643a8dae72205ef83317f852b447bdc4002984f
wasimGhoniem <wasimghoniem@gmail.com>
2024-12-31 19:47:36 +0200

    Added appBar in scaffold.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,7 +6,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text('git'),
+      ),
     );
   }
 }
